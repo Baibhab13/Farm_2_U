74830517549c72edd137f4e78509951efc506a09
Baibhab Sahu <147395059+Baibhab13@users.noreply.github.com>
2024-08-27 08:25:57 +0530

    Update Sell_page.kt

--- app/src/main/java/com/example/farm_2_u/pages/Sell_page.kt
+++ app/src/main/java/com/example/farm_2_u/pages/Sell_page.kt
@@ -1,14 +1,23 @@
 package com.example.farm_2_u.pages
 
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.example.farm_2_u.Homepage
 import com.example.farm_2_u.TopBar
 
 
 @Composable
 fun Sell_page(navController: NavHostController) {
     TopBar()
+    Button(
+        onClick = {
+            navController.navigate(Homepage.route)
+        }
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
