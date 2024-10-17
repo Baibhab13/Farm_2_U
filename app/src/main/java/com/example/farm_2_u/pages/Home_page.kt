37098234b60f264959a797e2a9b9e3423f6b51c1
Baibhab Sahu <147395059+Baibhab13@users.noreply.github.com>
2024-10-17 22:46:42 +0530

    Get otp visual transformation

--- app/src/main/java/com/example/farm_2_u/pages/Home_page.kt
+++ app/src/main/java/com/example/farm_2_u/pages/Home_page.kt
@@ -2,24 +2,11 @@ package com.example.farm_2_u.pages
 
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.farm_2_u.LazyGrid
+
+
 
 @Composable
 fun Home_page(navController: NavHostController) {
-    AppScreen()
-}
 
-@Composable
-private fun AppScreen() {
-    LazyGrid()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomePreview() {
-    val navController = rememberNavController()
-    Home_page(navController)
 }
